7440ec28a6aac493c8fcd06c82c5bdf8ae5b36c8
Jason Schmidt <jasonaschmidt1224@gmail.com>
2014-11-02 18:31:38 -0500

    Added particle.h class

---
+++ GraphicsProject/GraphicsProject/particle.h
@@ -0,0 +1,18 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "vertex.h"
+
+class Particle
+{
+public:
+	Vertex P;
+	Vector3f X, V, A;
+	void Draw(Vertex*, int&);
+	void Step(bool *k, int dt);
+	Particle(Vector3f, Vector4f);
+	Particle();
+	float Rand1();
+};
+
+#endif
