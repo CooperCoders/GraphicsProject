01b9876c6e5eb98751fa98b797128157d7c9d0bf
Jason Schmidt <jasonaschmidt1224@gmail.com>
2014-11-03 18:10:40 -0500

    Added Color stuff

--- GraphicsProject/GraphicsProject/particle.cpp
+++ GraphicsProject/GraphicsProject/particle.cpp
@@ -8,6 +8,7 @@ Particle::Particle()
 	V = Vector3f(rand() / (float)RAND_MAX - .5, rand() / (float)RAND_MAX - .5, 0);
 	A = Vector3f(0, -1, 0);
 	P = Vertex(X, Vector4f(.9, 0, 0, rand() / (float)RAND_MAX));
+	ColorSpeed = 30;
 }
 
 Particle::Particle(Vector3f x, Vector4f c)
@@ -22,13 +23,20 @@ void Particle::Step(bool* k, int* m, int _x, int _y, int dt)
 {
 	V += A * (dt / 1000.0f);
 	X += V * (dt / 1000.0f);
-	V *= .999;
+	V *= .99;
+	ColorAngle += ColorSpeed * (dt / 1000.0f);
 	
 	P.X = X;
+	P.Color = GetColor(ColorAngle, 1.0f);
 	if (X.y < -1)
 	{
 		V.y *= -.99;
 		X.y = -.999;
+	}
+	if (X.y > 1)
+	{
+		V.y *= -.99;
+		X.y = .999;
 	}
 	if (X.x < -1)
 	{
@@ -43,15 +51,55 @@ void Particle::Step(bool* k, int* m, int _x, int _y, int dt)
 	if (!m[0])
 	{
 		A = Vector3f((_x - 320) / 320.0f, (_y - 320) / -320.0f, 0) - X;
-		A *= 1 / (sqrt(A.x * A.x + A.y * A.y));
+		A *= 10 / (sqrt(A.x * A.x + A.y * A.y));
 	}
 	else
 	{
-		A = Vector3f(0, -1, 0);
+		A = Vector3f(0, -.2, 0);
 	}
 }
 
 void Particle::Draw(Vertex* buf, int& count)
 {
 	buf[count++] = P;
+}
+
+Vector4f Particle::GetColor(float angle, float alpha)
+{
+	float R, G, B;
+	float H = angle;
+	float S = 1;
+	float V = 1;
+	float C = V * S;
+	float Hp = H / 60;
+	float X = C*(1 - fabs((fmod(Hp, 2)) - 1));
+	if (Hp < 1)
+	{
+		R = C; G = X; B = 0;
+	}
+	else if (Hp < 2)
+	{
+		R = X; G = C; B = 0;
+	}
+	else if (Hp < 3)
+	{
+		R = 0; G = C; B = X;
+	}
+	else if (Hp < 4)
+	{
+		R = 0; G = X; B = C;
+	}
+	else if (Hp < 5)
+	{
+		R = X; G = 0; B = C;
+	}
+	else
+	{
+		R = C; G = 0; B = X;
+	}
+	float m = V - C;
+	R += m;
+	G += m;
+	B += m;
+	return Vector4f(R, G, B, alpha);
 }
--- GraphicsProject/GraphicsProject/particle.h
+++ GraphicsProject/GraphicsProject/particle.h
@@ -10,8 +10,11 @@ public:
 	Vector3f X, V, A;
 	void Draw(Vertex*, int&);
 	void Step(bool *k, int* m, int _x, int _y, int dt);
+	int ColorSpeed;
+	float ColorAngle;
 	Particle(Vector3f, Vector4f);
 	Particle();
+	Vector4f GetColor(float, float);
 };
 
 #endif
